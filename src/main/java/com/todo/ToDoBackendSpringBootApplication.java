5eb8e77304cc557e6b24d2b150a1f697f73184a6
Ahmed Tiba <ahmed.tiba.1993@gmail.com>
2022-06-09 12:20:08 +0100

    Initial commit

---
+++ src/main/java/com/todo/ToDoBackendSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoBackendSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoBackendSpringBootApplication.class, args);
+	}
+
+}
