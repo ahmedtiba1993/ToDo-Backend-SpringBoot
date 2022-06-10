49a00874a5d1c5bb73637ee5b0bb101c3a37c523
Ahmed Tiba <ahmed.tiba.1993@gmail.com>
2022-06-10 15:50:10 +0100

    Create TodoValidator.java

---
+++ src/main/java/com/todo/validator/TodoValidator.java
@@ -0,0 +1,39 @@
+package com.todo.validator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.util.StringUtils;
+
+import com.todo.model.dto.TodoDto;
+
+public class TodoValidator {
+	
+	public static List<String> validate(TodoDto dto){
+		
+		List<String> errors = new ArrayList<String>();
+		
+		if(dto == null){
+			
+			errors.add("Veillez renseigner le libelle");
+			errors.add("Veillez renseigner la description");
+			errors.add("la date est obligatoire");
+
+		}
+		
+		if(StringUtils.hasLength(dto.getLibelleTodo())) {
+			errors.add("Veillez renseigner le libelle");
+		}
+		
+		if(StringUtils.hasLength(dto.getDescriptionTodo())) {
+			errors.add("Veillez renseigner la description");
+		}
+		
+		if(dto.getDateTodo() == null) {
+			errors.add("la date est obligatoire");
+		}
+		
+		return errors;
+	}
+
+}
